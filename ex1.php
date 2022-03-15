33abbd583999d1a11558a35cf92a8e00edfb42bc
dm161272 <58472537+dm161272@users.noreply.github.com>
2022-03-15 22:18:47 +0100

    Add files via upload

---
+++ ex1.php
@@ -0,0 +1,24 @@
+
+
+<?php
+/*- Exercises 1
+Define a variable of each type: integer, double, string, and boolean. Then print them on the screen.*/
+
+$int = 1;
+$doub = 3.1456;
+$str = "test";
+$checker = true;
+
+echo "<hr>";
+
+echo "Var integer: $int<br>";
+
+echo "Var double: $doub<br>";
+
+echo "Var string: $str<br>";
+
+echo "Var boolean: $checker<br>";
+
+echo "<hr>";
+
+?>
